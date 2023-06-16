4ffd5f0940a88950ef13ce9fbf06c9777ec8ad9a
Creatlydev <creatlydev@gmail.com>
2023-06-16 05:31:50 -0500

    Creacion de script para convertir varias imagenes en un solo archivo pdf

---
+++ convert_to_pdf.py
@@ -0,0 +1,54 @@
+
+import os
+from PIL import Image
+from fpdf import FPDF
+
+
+
+
+def convert_images_to_pdf(image_dir, output_path):
+    # Obtener la lista de archivos en el directorio de imágenes
+    image_files = [f for f in os.listdir(
+        image_dir) if f.endswith(('.jpg', '.png', '.jpeg'))]
+
+    # Ordenar los archivos de imágenes alfabéticamente
+    image_files.sort()
+
+    # Crear el objeto FPDF
+    pdf = FPDF()
+
+    # Recorrer cada archivo de imagen y agregarlo al PDF
+    for image_file in image_files:
+        # Abrir la imagen utilizando PIL
+        image_path = os.path.join(image_dir, image_file)
+        image = Image.open(image_path)
+
+        # Obtener el tamaño de la imagen
+        image_width, image_height = image.size
+
+        # Calcular el tamaño máximo del PDF
+        max_width = 210  # Ancho de página A4 en mm
+        max_height = 297  # Alto de página A4 en mm
+
+        # Calcular la escala de la imagen para ajustarla dentro de la página PDF
+        scale = min(max_width / image_width, max_height / image_height)
+
+        # Calcular la posición centrada de la imagen en la página PDF
+        x = (max_width - (image_width * scale - 30)) / 2
+        y = (max_height - (image_height * scale - 30)) / 2
+
+        # Agregar una nueva página al PDF
+        pdf.add_page()
+
+        # Agregar la imagen al PDF, centrada
+        pdf.image(image_path, x, y, image_width * scale - 30, image_height * scale - 30)
+
+    # Guardar el PDF resultante
+    pdf.output(output_path, 'F')
+    print(f'Se ha creado el PDF: {output_path}')
+
+
+# Ejemplo de uso
+image_directory = 'D:/samir/Pictures/Saved Pictures'
+output_pdf_path = 'resultado.pdf'
+convert_images_to_pdf(image_directory, output_pdf_path)
